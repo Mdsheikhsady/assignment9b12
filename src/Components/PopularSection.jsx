b5cd7e01baec2a046fe8cb4b7a4810810fb6a2fd
MdSheikhSady <sadysks2020@gmail.com>
2025-11-25 08:16:10 +0600

    first commit

---
+++ src/Components/PopularSection.jsx
@@ -0,0 +1,47 @@
+import React, { useEffect, useState } from "react";
+
+const PopularSection = () => {
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    fetch("services.json")
+      .then((res) => res.json())
+      .then((data) => setServices(data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <div className="px-20 mt-8">
+      <div >
+        <h3 className="font-bold text-3xl text-center">Popular Section</h3>
+      </div>
+
+      {/* card */}
+      <div className="grid grid-cols-3 gap-10 mt-12">
+        {services.map((service) => (
+          <div className="card bg-base-100 w-96 shadow-sm">
+            <figure>
+              <img className="w-full h-70 object-cover"
+                src={service?.image}
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">{service?.serviceName}</h2>
+              <div className="flex justify-between">
+                <p>Price: {service?.price} $</p>
+                <p>Rating: {service?.rating}</p>
+
+              </div>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">View Details</button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default PopularSection;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Slider from '../Components/Slider';
+import PopularSection from '../Components/PopularSection';
+
+const Home = () => {
+    return (
+        <div>
+            <Slider></Slider>
+            <PopularSection></PopularSection>
+        </div>
+    );
+};
+
+export default Home;
